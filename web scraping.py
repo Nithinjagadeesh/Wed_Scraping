ec871ccc634188262c255fa03ed4d56c1ec16531
Nithinjagadeesh <92904088+Nithinjagadeesh@users.noreply.github.com>
2025-06-12 13:40:56 +0530

    Delete web scraping.py

--- web scraping.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-url= input("Enter the URL to scrape:")
-response=requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-links = soup.find_all('a')
-for link in links:
-    print(link.get('href'))
